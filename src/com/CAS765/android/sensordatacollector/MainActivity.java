fdf0522dfc3016588fc37548c92b8c70dd748d48
kevlarjacket <thekevlarjacket@gmial.com>
2013-11-28 23:44:34 -0500

    removed most of the step counting code.

--- src/com/CAS765/android/sensordatacollector/MainActivity.java
+++ src/com/CAS765/android/sensordatacollector/MainActivity.java
@@ -364,114 +364,92 @@ public class MainActivity extends Activity implements SensorEventListener {
 		double accelY = event.values[1];
 		double accelZ = event.values[2];
 
-		double accelMag = Math.sqrt(Math.pow(accelX,2) + Math.pow(accelY,2) + Math.pow(accelZ,2));
 		
-		// save current acceleration value.
-		accelData.add(accelMag);
-		//mStepCount.setText(Double.toString(accelMag));
-	
-		if (accelData.size() > MAXDATA)
-			accelData.remove(0);
-		else
-			return; 		// 
 		
-		List<Double> x = new  LinkedList<Double>();		// Array of normalized auto-correlation values for this sample
-		double numerator, denominator;
-		numerator=0;
-		//Log.d("TMINMAX","TMIN="+Tmin+" TMAX="+Tmax);
-		for (T = Tmin; T <= Tmax; T++)		// Loop through search window
-		{
-			double totalNumerator = 0;
-			double mean1 = mean(0,T),
-					mean2 = mean(T,T);
-			//Log.d("Means", "Mean 1: "+mean1+" Mean 2:"+mean2);
-			for (int k = 0; k<= T-1; k++)	// Calculate numerator sum, mean still needs to be done
-			{
-				numerator = (accelData.get(k) - mean1) * (accelData.get(k+T) - mean2); 	
-				//Log.d("K", "K is "+k);
-				totalNumerator = totalNumerator + numerator;
-			}
-
-			denominator = T * (stdDev(0,T,mean1)) * (stdDev(T,T,mean2));	//  Calculate denominator, stdDev needs to be figured out
-			x.add( totalNumerator / denominator);	// Put values of x in arrayList
-			//Log.d("","T #"+T+" Completed");
-		}
-		double v=0;
-		//Log.d("XSIZE",""+x.size());
-		for (int i = 0; i < x.size(); i++ )	// Find maximum value in x, and set v(m) and Topt accordingly
-		{
-			//Log.d("X", "X is "+x.get(i));
-			if (Math.abs(x.get(i)) >v)
-			{
-			//	Log.d("NEW V","OLD V is "+v+" NEW V is "+x.get(i)); 
-				v =  Math.abs(x.get(i));
-				Topt = i + Tmin;
-			}
-		}
-		//Log.d("V", "V is "+v);
-		Tmin = Topt - 10;				// Set Tmin / Tmax, in paper they used +/-10
-		if (Tmin<=40) {Tmin=40;}
-		Tmax = Topt + 10;
-		if (Tmax>=100) {Tmax=100;}
-			
-		
-		if (stdDev(0,DEFAULT_SAMPLING_RATE+1,mean(0,DEFAULT_SAMPLING_RATE+1)) < 0.01*9.8)		// If standard deviation of current data is <0.01, set state back to IDLE
-		{
-			state = INACTIVE;
-			samplesWalking = 0;			// Also reset samples spent walking
-		}
-		else if (v > 0.7)
-		{
-			state = WALKING;			// If v for current sample is above 0.7, state is now WALKING
-		}
-		
-		//Log.d("STATE","State "+state);
-		if (state == WALKING)			// When in walking state, do this
-		{
-				samplesWalking++;			// Increment samples spent walking
-				if (samplesWalking > (Topt/2))		// 
-				{
-					steps++;
-					samplesWalking = 0;
-					//mStepCount.setText(""+steps);
-				}
-		}	
-		
-		if (displayInterval >=50)
-		{
-			displayInterval=0;
-			mTOpt.setText("T Optimal: "+Topt);
-			mMaxAuto.setText("Max Autocorrelation: "+v);
-			mStdDev.setText("Std Dev: "+(stdDev(0,DEFAULT_SAMPLING_RATE+1,mean(0,DEFAULT_SAMPLING_RATE+1))/9.8)+"g");
-			if (state==INACTIVE) mCurState.setText("State=INACTIVE");
-			if (state==WALKING) mCurState.setText("State=WALKING");
-			mAccelMag.setText("Mean Accel"+mean(0,DEFAULT_SAMPLING_RATE+1));
-			
-			
-		}
+//		double accelMag = Math.sqrt(Math.pow(accelX,2) + Math.pow(accelY,2) + Math.pow(accelZ,2));
+//		
+//		// save current acceleration value.
+//		accelData.add(accelMag);
+//		//mStepCount.setText(Double.toString(accelMag));
+//	
+//		if (accelData.size() > MAXDATA)
+//			accelData.remove(0);
+//		else
+//			return; 		// 
+//		
+//		List<Double> x = new  LinkedList<Double>();		// Array of normalized auto-correlation values for this sample
+//		double numerator, denominator;
+//		numerator=0;
+//		//Log.d("TMINMAX","TMIN="+Tmin+" TMAX="+Tmax);
+//		for (T = Tmin; T <= Tmax; T++)		// Loop through search window
+//		{
+//			double totalNumerator = 0;
+//			double mean1 = mean(0,T),
+//					mean2 = mean(T,T);
+//			//Log.d("Means", "Mean 1: "+mean1+" Mean 2:"+mean2);
+//			for (int k = 0; k<= T-1; k++)	// Calculate numerator sum, mean still needs to be done
+//			{
+//				numerator = (accelData.get(k) - mean1) * (accelData.get(k+T) - mean2); 	
+//				//Log.d("K", "K is "+k);
+//				totalNumerator = totalNumerator + numerator;
+//			}
+//
+//			denominator = T * (stdDev(0,T,mean1)) * (stdDev(T,T,mean2));	//  Calculate denominator, stdDev needs to be figured out
+//			x.add( totalNumerator / denominator);	// Put values of x in arrayList
+//			//Log.d("","T #"+T+" Completed");
+//		}
+//		double v=0;
+//		//Log.d("XSIZE",""+x.size());
+//		for (int i = 0; i < x.size(); i++ )	// Find maximum value in x, and set v(m) and Topt accordingly
+//		{
+//			//Log.d("X", "X is "+x.get(i));
+//			if (Math.abs(x.get(i)) >v)
+//			{
+//			//	Log.d("NEW V","OLD V is "+v+" NEW V is "+x.get(i)); 
+//				v =  Math.abs(x.get(i));
+//				Topt = i + Tmin;
+//			}
+//		}
+//		//Log.d("V", "V is "+v);
+//		Tmin = Topt - 10;				// Set Tmin / Tmax, in paper they used +/-10
+//		if (Tmin<=40) {Tmin=40;}
+//		Tmax = Topt + 10;
+//		if (Tmax>=100) {Tmax=100;}
+//			
+//		
+//		if (stdDev(0,DEFAULT_SAMPLING_RATE+1,mean(0,DEFAULT_SAMPLING_RATE+1)) < 0.01*9.8)		// If standard deviation of current data is <0.01, set state back to IDLE
+//		{
+//			state = INACTIVE;
+//			samplesWalking = 0;			// Also reset samples spent walking
+//		}
+//		else if (v > 0.7)
+//		{
+//			state = WALKING;			// If v for current sample is above 0.7, state is now WALKING
+//		}
+//		
+//		//Log.d("STATE","State "+state);
+//		if (state == WALKING)			// When in walking state, do this
+//		{
+//				samplesWalking++;			// Increment samples spent walking
+//				if (samplesWalking > (Topt/2))		// 
+//				{
+//					steps++;
+//					samplesWalking = 0;
+//					//mStepCount.setText(""+steps);
+//				}
+//		}	
+//		
+//		if (displayInterval >=50)
+//		{
+//			displayInterval=0;
+//			mTOpt.setText("T Optimal: "+Topt);
+//			mMaxAuto.setText("Max Autocorrelation: "+v);
+//			mStdDev.setText("NOTHING");
+//			if (state==INACTIVE) mCurState.setText("State=INACTIVE");
+//			if (state==WALKING) mCurState.setText("State=WALKING");
+//			mAccelMag.setText("NOTHING");
+//			
+//			
+//		}
 	}
-	
-	public double mean (int start, int range)
-	{
-		double sum=0;
-		for(int i=0;i<range;i++)
-		{
-			sum+=accelData.get(start+i);
-		}
-		return sum*1.0/range;
-	}
-	public double stdDev(int start, int range, double mean)
-	{
-		double sum=0;
-		//double mean= mean(start,range);
-		int n=0;
-		for(int i=0;i<range;i++)
-		{
-			sum+=(accelData.get(start+i)-mean)*(accelData.get(start+i)-mean);
-			n++;
-		}
-		return Math.sqrt(sum/n);
-	}
-
-
 }
